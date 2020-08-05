1779f88aa2e2667834821c83fdb2a49a0ff74214
Wei <50000741+ZHANGW31@users.noreply.github.com>
2020-08-05 15:02:53 -0700

    Adding latest build

--- WordBankHeist/src/com/wordbank/client/Game.java
+++ WordBankHeist/src/com/wordbank/client/Game.java
@@ -44,18 +44,29 @@ public class Game {
         player.setName(name);
         int playerLives = player.getCurrentLives();
         int playerCash = player.getCash();
-        int awardedCash = 0;
+        int awardedCash;
         int answeredWrong = 0;
         int playerStartingLife = player.getCurrentLives();
+        int consecutiveCorrectAnswers = 0;
+        boolean cashOut = false;
 
         //WordBank MainWords Setup
         Set<String> mainWords = wordBankCollection.getMainWords();
 
-        //CashOut option
-        boolean cashOut = false;
         System.out.println(prompter.welcomeMessage() + name + "\n" + prompter.beginningMessage());
-        System.out.println("HackAssistAI: Initializing hack...injecting package... loading... success!");
+        String viewTheRules = scanner.nextLine().toLowerCase();
+        if (viewTheRules.equals("y")){
+            try (BufferedReader reader = new BufferedReader(new FileReader("Rules of the Game.txt"))) {
+                Stream<String> line = reader.lines();
+                line.forEach(System.out::println);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
+        Thread.sleep(2000);
+        System.out.println("HackAssistAI: Initializing hack...injecting package... loading... success!");
+        Thread.sleep(2000);
 
         //Todo create the rules and objectives.
 
@@ -64,12 +75,21 @@ public class Game {
             //Game logic starts here
             String word = questionFactory.getRandomWord(mainWords);
             Set<String> answerKey = questionFactory.answerKey(word.length(),word.charAt(0),mainWords);
-
+            Thread.sleep(2000);
             //Beginning Phase
+
+            if (consecutiveCorrectAnswers == 5){
+                playerLives += 1;
+                consecutiveCorrectAnswers = 0;
+            }
             System.out.println(prompter.cashOutMessage(playerCash,playerLives));
 
+
             //Question Phase
+            Thread.sleep(2000);
             System.out.println(questionFactory.questionGenerator(word));
+
+
 
             //User Answer Phase
             String playerAnswer = scanner.nextLine().toLowerCase();
@@ -77,21 +97,28 @@ public class Game {
                 mainWords.remove(playerAnswer);
                 awardedCash = 100 * playerAnswer.length();
                 playerCash += awardedCash;
+                consecutiveCorrectAnswers++;
                 System.out.println(prompter.rightAnswerMessage() + prompter.rightAnswerCashAmount(awardedCash));
+                Thread.sleep(1500);
 
             } else { //If the user's answer is incorrect.
 
                 System.out.println(prompter.wrongAnswerMessage());
+                Thread.sleep(1000);
+                consecutiveCorrectAnswers = 0;
                 playerLives--;
                 answeredWrong++;
             }
             if (answeredWrong > playerStartingLife){
+                consecutiveCorrectAnswers = 0;
                 System.out.println(prompter.endOfTryMessage());
+                Thread.sleep(1000);
                 break;
             }
 
             //Options to cash out Phase
             System.out.println(prompter.wrongAnswerTryTokenConsumed(playerLives));
+            Thread.sleep(1000);
             System.out.println(prompter.cashOutOptionMessage());
             String playerChoice = scanner.nextLine().toLowerCase();
                 if (playerChoice.equals("y")){
@@ -100,6 +127,7 @@ public class Game {
                 } else if (playerChoice.equals("n")){
 
                     System.out.println(prompter.continueMessage());
+                    Thread.sleep(1500);
                 }
 
         }
